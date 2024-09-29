f3ccde6db5db65f7078a3c87b964607a18662ff3
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2024-09-29 15:36:33 -0300

    initial commit

---
+++ reserva-avaliacao-restaurante/src/main/java/br/com/reserva/avaliacao/restaurante/ReservaAvaliacaoRestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.reserva.avaliacao.restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaAvaliacaoRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaAvaliacaoRestauranteApplication.class, args);
+	}
+
+}
---
+++ reserva-avaliacao-restaurante/src/test/java/br/com/reserva/avaliacao/restaurante/ReservaAvaliacaoRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.reserva.avaliacao.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaAvaliacaoRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
