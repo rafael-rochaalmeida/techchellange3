5e92838247857f64c8d27f1fbc66c55d99660cf3
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2024-09-30 15:25:12 -0300

    config conexao com mongodb

--- reserva-avaliacao-restaurante/src/main/java/br/com/reserva/avaliacao/restaurante/ReservaAvaliacaoRestauranteApplication.java
+++ reserva-avaliacao-restaurante/src/main/java/br/com/reserva/avaliacao/restaurante/ReservaAvaliacaoRestauranteApplication.java
@@ -2,8 +2,12 @@ package br.com.reserva.avaliacao.restaurante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ReservaAvaliacaoRestauranteApplication {
 
 	public static void main(String[] args) {
--- reserva-avaliacao-restaurante/src/main/java/br/com/reserva/avaliacao/restaurante/config/DbConfig.java
+++ reserva-avaliacao-restaurante/src/main/java/br/com/reserva/avaliacao/restaurante/config/DbConfig.java
@@ -2,83 +2,38 @@ package br.com.reserva.avaliacao.restaurante.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
-import javax.sql.DataSource;
-import java.sql.SQLException;
-
-import oracle.ucp.jdbc.PoolDataSource;
-import oracle.ucp.jdbc.PoolDataSourceFactory;
 
 @Configuration
 public class DbConfig {
 
-    @Value("${api.datasource.url}")
-    private String jdbcUrl;
+    @Value("${api.datasource.host}")
+    private String host;
 
-    @Value("${api.datasource.uid}")
-    private String uid;
+    @Value("${api.datasource.port}")
+    private String port;
 
-    @Value("${api.datasource.pwd}")
-    private String pwd;
+    @Value("${api.datasource.database}")
+    private String database;
 
-    @Value("${api.datasource.connectionPoolName}")
-    private String connectionPoolName;
+    @Value("${api.datasource.username}")
+    private String username;
 
-    @Value("${api.datasource.minPoolSize}")
-    private int minPoolSize;
-
-    @Value("${api.datasource.maxPoolSize}")
-    private int maxPoolSize;
-
-    @Value("${api.datasource.initialPoolSize}")
-    private int initialPoolSize;
-
-    @Value("${api.datasource.inactiveConnectionTimeout}")
-    private int inactiveConnectionTimeout;
-
-    @Value("${api.datasource.connectionWaitTimeout}")
-    private int connectionWaitTimeout;
-
-    @Value("${api.datasource.timeoutCheckInterval}")
-    private int timeoutCheckInterval;
-
-    @Value("${api.datasource.timeToLiveConnectionTimeout}")
-    private int timeToLiveConnectionTimeout;
-
-    @Value("${api.datasource.queryTimeout}")
-    private int queryTimeout;
-
-    @Value("${api.datasource.abandonedConnectionTimeout}")
-    private int abandonedConnectionTimeout;
-
-    @Value("${api.datasource.sessionName}")
-    private String sessionName;
-
-    // @Value("${api.datasource.schema}")
-    // private String schema;
+    @Value("${api.datasource.password}")
+    private String password;
 
     @Bean
-    public DataSource getOracleDataSource() throws SQLException {
+    public MongoDatabaseFactory getMongoDataSource() {
+        String uri = String.format("mongodb://%s:%s@%s:%s/%s", username, password, host, port, database);
+        return new SimpleMongoClientDatabaseFactory(uri);
+    }
 
-        PoolDataSource pds = PoolDataSourceFactory.getPoolDataSource();
-
-        pds.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
-        pds.setURL(jdbcUrl);
-		pds.setUser(uid);
-		pds.setPassword(pwd);
-		pds.setConnectionPoolName(connectionPoolName);
-		pds.setMinPoolSize(minPoolSize);
-		pds.setMaxPoolSize(maxPoolSize);
-		pds.setInitialPoolSize(initialPoolSize);
-		pds.setTimeToLiveConnectionTimeout(timeToLiveConnectionTimeout);
-		pds.setQueryTimeout(queryTimeout);
-		pds.setInactiveConnectionTimeout(inactiveConnectionTimeout);
-		pds.setAbandonedConnectionTimeout(abandonedConnectionTimeout);
-		pds.setConnectionWaitTimeout(connectionWaitTimeout);
-		pds.setTimeoutCheckInterval(timeoutCheckInterval);
-		pds.setConnectionProperty("v$session.program", sessionName);
-        
-        return pds;
+       @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(getMongoDataSource());
     }
 }
+
